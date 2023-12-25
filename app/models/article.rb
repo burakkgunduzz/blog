af72976d30c46f8137f0a3d7f9dd86410ef17106
burakkgunduzz <burakkgunduz21@gmail.com>
2023-12-25 14:00:20 +0300

    add redis for action cable and use turbo frames for articles

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,9 @@
 class Article < ApplicationRecord
   include Visible
-
+  after_create_commit { broadcast_prepend_to 'articles' }
+  after_update_commit { broadcast_replace_to "articles" }
+  after_destroy_commit { broadcast_remove_to "articles" }
+  
   has_many :comments, dependent: :destroy
 
   validates :title, presence: true
